181d0fef2556dcd5257a2902d2999910b3ef8e54
bshongwe <dru.da.beat.hustle@gmail.com>
2024-10-12 10:17:05 +0200

    Update sign in CORS

--- src/pages/api/auth/signup.js
+++ src/pages/api/auth/signup.js
@@ -1,11 +1,33 @@
 import authService from "../../../services/authService"; // Adjust the import path as needed
+import Cors from 'cors'; // for middleware
+
+// Initialize the cors middleware
+const cors = Cors({
+  methods: ['POST', 'OPTIONS'],
+  origin: '*', // Adjust the origin as needed
+});
+
+// Helper method to wait for a middleware to execute before continuing
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
   if (req.method === "POST") {
     const { fullName, email, password } = req.body;
 
     try {
-      // Implement your signup logic here
+      // Signup logic
       await authService.signUp(fullName, email, password);
       return res.status(200).json({ message: "User registered successfully." });
     } catch (error) {
@@ -13,7 +35,7 @@ export default async function handler(req, res) {
     }
   } else {
     // Method Not Allowed
-    res.setHeader("Allow", ["POST"]);
+    res.setHeader("Allow", ["POST", "OPTIONS"]);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
